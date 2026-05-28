21ad46e84af2438d3367005cbb05e94f3e247e22
kamolovairoda885-sudo <kamolovairoda885@gmail.com>
2026-05-28 12:07:40 +0500

    Delete main.py

--- main.py
+++
@@ -1,6 +0,0 @@
-async def main():
-    init_db()
-
-    scheduler = AsyncIOScheduler(timezone="Asia/Tashkent")  # ← shunday bo'lishi kerak
-    scheduler.add_job(send_daily_report, "cron", hour=20, minute=0)
-    scheduler.start()
